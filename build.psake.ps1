57e1376a7366ddb14a2270a521c66910d43332c1
Dave Wyatt <dlwyatt115@gmail.com>
2015-04-11 17:34:08 -0400

    Digital signatures updated to use SHA256 instead of SHA1

--- build.psake.ps1
+++ build.psake.ps1
@@ -83,9 +83,10 @@ Task Sign {
     )
 
     $splat = @{
-        Certificate  = $cert
-        IncludeChain = 'All'
-        Force        = $true
+        Certificate   = $cert
+        IncludeChain  = 'All'
+        Force         = $true
+        HashAlgorithm = 'SHA256'
     }
 
     if ($signerTimestampUrl) { $splat['TimestampServer'] = $signerTimestampUrl }
